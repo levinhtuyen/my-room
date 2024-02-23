72d2d30aadc622c4c0e0a9a6d7135c7138c2a4ae
tuyen-go2joy <levinhtuyen210@gmail.com>
2024-02-23 14:01:19 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'production',
   devtool: 'source-map',
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
