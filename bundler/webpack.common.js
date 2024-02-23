d2b7dabc41f4421eca418b578368bcbd30330878
tuyen-go2joy <levinhtuyen210@gmail.com>
2024-02-23 14:00:05 +0700

    Update webpack.common.js

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -3,7 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
