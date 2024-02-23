e54d8edb487d88f4acb0b33c590e08edd5e1a4b1
tuyen-go2joy <levinhtuyen210@gmail.com>
2024-02-23 13:46:50 +0700

    Update webpack.prod.js

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = merge(
     commonConfiguration,
     {
-        mode: 'development',
+        mode: 'production',
         plugins:
         [
             new CleanWebpackPlugin()
